60a675e0c588999df3b4807e49a016d05f8b1b69
Guidotss <guidoolguin@hotmail.com>
2023-06-23 13:59:01 -0300

    Initial commit

---
+++ style-guide.md
@@ -0,0 +1,38 @@
+# Front-end Style Guide
+
+## Layout
+
+The designs were created to the following widths:
+
+- Mobile: 375px
+- Desktop: 1440px
+
+## Colors
+
+### Primary
+
+- Blue: #7BCC99
+- Light red (work): #FFAA00
+- Soft blue (play): #60BC9E
+- Light red (study): #C60068
+- Lime green (exercise): #4A8E70
+- Violet (social): #464EC2
+- Soft orange (self care): #E09715
+
+### Neutral
+
+- Very dark blue:  #1A1E29
+- Dark blue: #162844
+- Desaturated blue: #57A5CC
+- Pale Blue: #F2F2F2
+
+## Typography
+
+### Body Copy
+
+- Font size: 18px (card titles e.g. Work, Play)
+
+### Font
+
+- Family: [Rubik](https://fonts.google.com/specimen/Rubik)
+- Weights: 300, 400, 500
--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,19 @@ module.exports = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      colors: {
+        'light_red':           '#FFAA00',
+        'light_red':           '#C60068',
+        'soft_orange':         '#E09715',
+        'lime_green':          '#4A8E70',
+        'violet':              '#464EC2',
+        'blue':                '#7BCC99',
+        'soft_blue':           '#60BC9E',
+        'dark_blue':           '#162844',
+        'pale_blue':           '#F2F2F2',
+        'very_dark_blue':      '#1A1E29',
+        'desaturated_blue':    '#57A5CC',
+      }
     },
   },
   plugins: [],
