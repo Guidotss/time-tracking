ff5b467fea97b2b29acd34524edb1ddd3ab4edbc
Guidotss <guidoolguin@hotmail.com>
2023-06-24 15:23:43 -0300

    Terminando login de usuario

---
+++ src/app/api/auth/login/route.ts
@@ -0,0 +1,25 @@
+import "@/database/connect";
+import { UserService } from "@/services";
+
+const userService = new UserService();
+
+export async function POST(req: Request) {
+  const { name, password } = await req.json();
+  if (!name || !password) throw new Error("Missing name or password");
+
+  try {
+    const user = await userService.login(name, password);
+    if (!user) return new Response("User does not exist", { status: 404 });
+    return new Response(
+      JSON.stringify({
+        message: "User login successfully",
+        name: user.name,
+        activity: user.activity,
+      }),
+      { status: 200 }
+    );
+  } catch (error) {
+    console.log(error);
+    return new Response("Error login user", { status: 500 });
+  }
+}
--- src/services/userService.ts
+++ src/services/userService.ts
@@ -8,6 +8,10 @@ export class UserService {
     return await bcypt.hash(password, 10);
   }
 
+  private async comparePassword(password: string, hashedPassword: string) {
+    return await bcypt.compare(password, hashedPassword);
+  }
+
   public async createUser(name: string, password: string) {
     try {
       const hashedPassword = await this.hashPassword(password);
@@ -20,4 +24,29 @@ export class UserService {
       throw new Error("Error creating user");
     }
   }
+
+
+  public login = async (name: string, password: string) => {
+    try{
+      
+      const user = await this.userModel.findOne({ name }).lean();
+      if (!user) {
+        throw new Error("User does not exist");
+      }
+      
+      const validPassword = await this.comparePassword(password, user.password);
+      if (!validPassword) {
+        throw new Error("Invalid password");
+      }
+      return user;
+
+
+    }catch(error){
+      console.log(error);
+      throw new Error("Error login user");
+    }
+  }
+
+
+
 }
